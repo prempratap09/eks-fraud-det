3c385c9b1253c5dea413090c5acd280f65d7141c
prempratap09 <58620626+prempratap09@users.noreply.github.com>
2022-04-27 19:02:10 +0530

    Update s3_utils.py

--- s3_utils.py
+++ s3_utils.py
@@ -10,7 +10,7 @@ s3 = boto3.client(
     "s3",
     aws_access_key_id= os.environ.get( "AWS_ACCESS_KEY_ID" ), 
     aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY" ), 
-    region_name=os.environ.get('region_name'),
+    region_name=os.environ.get('REGION_NAME'),
 )
 
 
